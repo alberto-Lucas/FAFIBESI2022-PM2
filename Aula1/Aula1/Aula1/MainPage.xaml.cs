9591b733aef2a435855e0ab633ce7ef5fc99db78
Lucas Alberto <lucas.alberto@linx.com.br>
2022-08-11 12:48:59 -0300

    Aula1

---
+++ Aula1/Aula1/Aula1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Aula1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Clicked(object sender, EventArgs e)
+        {
+            if (txtSenha.Text == "1")
+            DisplayAlert("Atenção", "Mensagem: " + txtSenha.Text, "Ok");
+        }
+    }
+}
